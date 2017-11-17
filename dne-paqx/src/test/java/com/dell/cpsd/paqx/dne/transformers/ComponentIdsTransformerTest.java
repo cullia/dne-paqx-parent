db4d31daa50db9110f3beef64253f68e1fd1e943
Kieran Murphy <Kieran.Murphy1@vce.com>
2017-11-17 19:49:04 +0000

    added tests for ComponentIdsTransformer

--- dne-paqx/src/test/java/com/dell/cpsd/paqx/dne/transformers/ComponentIdsTransformerTest.java
+++ dne-paqx/src/test/java/com/dell/cpsd/paqx/dne/transformers/ComponentIdsTransformerTest.java
@@ -5,7 +5,21 @@
 
 package com.dell.cpsd.paqx.dne.transformers;
 
-import static org.junit.Assert.*;
+import com.dell.cpsd.paqx.dne.repository.DataServiceRepository;
+import com.dell.cpsd.paqx.dne.service.model.ComponentEndpointIds;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 /**
  * Component Endpoint Ids Transformer Unit Test class
@@ -17,8 +31,105 @@ import static org.junit.Assert.*;
  * @version 1.0
  * @since 1.0
  */
-//TODO: Complete this
+@RunWith(MockitoJUnitRunner.class)
 public class ComponentIdsTransformerTest
 {
+    @Mock
+    private DataServiceRepository dataServiceRepository;
 
+    @Mock
+    private ComponentEndpointIds componentEndpointIds;
+
+    private ComponentIdsTransformer transformer;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        this.transformer = new ComponentIdsTransformer(this.dataServiceRepository);
+    }
+
+    @Test
+    public void testGetComponentEndpointIdsByComponentTypeIsValid() throws Exception
+    {
+        when(this.dataServiceRepository.getComponentEndpointIds(anyString())).thenReturn(this.componentEndpointIds);
+
+        final ComponentEndpointIds componentEndpointIdsByComponentType = this.transformer
+                .getComponentEndpointIdsByComponentType(anyString());
+
+        assertNotNull(componentEndpointIdsByComponentType);
+        assertSame(this.componentEndpointIds, componentEndpointIdsByComponentType);
+    }
+
+    @Test
+    public void testGetComponentEndpointIdsByComponentTypeException() throws Exception
+    {
+        when(this.dataServiceRepository.getComponentEndpointIds(anyString())).thenReturn(null);
+
+        try
+        {
+            this.transformer.getComponentEndpointIdsByComponentType(anyString());
+            fail("Expected exception to be thrown but was not");
+        }
+        catch (IllegalStateException ex)
+        {
+            assertThat(ex.getMessage(), containsString("No component ids found"));
+        }
+    }
+
+    @Test
+    public void testGetVCenterComponentEndpointIdsByEndpointTypeIsValid() throws Exception
+    {
+        when(this.dataServiceRepository.getVCenterComponentEndpointIdsByEndpointType(anyString())).thenReturn(this.componentEndpointIds);
+
+        final ComponentEndpointIds componentEndpointIdsByComponentType = this.transformer
+                .getVCenterComponentEndpointIdsByEndpointType(anyString());
+
+        assertNotNull(componentEndpointIdsByComponentType);
+        assertSame(this.componentEndpointIds, componentEndpointIdsByComponentType);
+    }
+
+    @Test
+    public void testGetVCenterComponentEndpointIdsByEndpointTypeException() throws Exception
+    {
+        when(this.dataServiceRepository.getVCenterComponentEndpointIdsByEndpointType(anyString())).thenReturn(null);
+
+        try
+        {
+            this.transformer.getVCenterComponentEndpointIdsByEndpointType(anyString());
+            fail("Expected exception to be thrown but was not");
+        }
+        catch (IllegalStateException ex)
+        {
+            assertThat(ex.getMessage(), containsString("No component endpoint ids found"));
+        }
+    }
+
+    @Test
+    public void testGetComponentEndpointIdsByCredentialTypeIsValid() throws Exception
+    {
+        when(this.dataServiceRepository.getComponentEndpointIds(anyString(), anyString(), anyString()))
+                .thenReturn(this.componentEndpointIds);
+
+        final ComponentEndpointIds componentEndpointIdsByComponentType = this.transformer
+                .getComponentEndpointIdsByCredentialType(anyString(), anyString(), anyString());
+
+        assertNotNull(componentEndpointIdsByComponentType);
+        assertSame(this.componentEndpointIds, componentEndpointIdsByComponentType);
+    }
+
+    @Test
+    public void testGetComponentEndpointIdsByCredentialTypeException() throws Exception
+    {
+        when(this.dataServiceRepository.getComponentEndpointIds(anyString(), anyString(), anyString())).thenReturn(null);
+
+        try
+        {
+            this.transformer.getComponentEndpointIdsByCredentialType(anyString(), anyString(), anyString());
+            fail("Expected exception to be thrown but was not");
+        }
+        catch (IllegalStateException ex)
+        {
+            assertThat(ex.getMessage(), containsString("No component ids found"));
+        }
+    }
 }
